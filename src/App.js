6c42ba3f7690af4493e074fc0444d5ccde87d36d
Danilo <danilorb20099@hotmail.com>
2019-08-25 13:20:45 -0600

    adding 404 page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route,Switch } from "react-router-dom";
 import index from "./pages/Index";
 import detailAlbum from "./pages/DetailAlbum"
 import mainPage from "./pages/MainPage";
 import artistDetail from "./pages/artisDetail";
 import History from "./history/History";
 import errorPageToken from "./components/errorPages/ErrorPageToken";
+import notFound from "./components/errorPages/NotFound";
 const App = () => {
   return (
     <Router history={History}>
+      <Switch>
       <Route path="/ArtistDetail/:id" exact component={artistDetail} />
       <Route path="/AlbumDetail/:id" exact component={detailAlbum} />
       <Route path="/MainPage/" exact component={mainPage} />
       <Route path="/Error/" exact component={errorPageToken} />
       <Route path="/" exact component={index} />
+       <Route component={notFound}/>
+       </Switch>
     </Router>
   );
 };
---
+++ src/components/errorPages/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Result } from 'antd';
+const NotFound=()=>{
+
+return(
+ <div>
+ <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+  />,
+ </div>
+)
+
+
+}
+
+export default NotFound;
